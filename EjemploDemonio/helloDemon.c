38eb1bc2baefe625ea2d3d1d2bbe42ab63e26231
Edward <edwarduw@gmail.com>
2016-08-12 19:39:51 -0600

    agrega monitoreo CPU y Memoria

--- EjemploDemonio/helloDemon.c
+++ EjemploDemonio/helloDemon.c
@@ -11,6 +11,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <syslog.h>
+#include <sys/types.h>
+#include <sys/sysinfo.h>
+
+struct sysinfo memInfo;
+static unsigned long long lastTotalUser, lastTotalUserLow, lastTotalSys, lastTotalIdle;
 
 static void skeleton_daemon()
 {
@@ -65,20 +70,115 @@ static void skeleton_daemon()
     openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
 }
 
+
+void init(){
+    FILE* file = fopen("/proc/stat", "r");
+    fscanf(file, "cpu %llu %llu %llu %llu", &lastTotalUser, &lastTotalUserLow,
+        &lastTotalSys, &lastTotalIdle);
+    fclose(file);
+}
+
+double getCurrentValue(){
+    double percent;
+    FILE* file;
+    unsigned long long totalUser, totalUserLow, totalSys, totalIdle, total;
+
+    file = fopen("/proc/stat", "r");
+    fscanf(file, "cpu %llu %llu %llu %llu", &totalUser, &totalUserLow,
+        &totalSys, &totalIdle);
+    fclose(file);
+
+    if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow ||
+        totalSys < lastTotalSys || totalIdle < lastTotalIdle){
+        //Overflow detection. Just skip this value.
+        percent = -1.0;
+    }
+    else{
+        total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) +
+            (totalSys - lastTotalSys);
+        percent = total;
+        total += (totalIdle - lastTotalIdle);
+        percent /= total;
+        percent *= 100;
+    }
+
+    lastTotalUser = totalUser;
+    lastTotalUserLow = totalUserLow;
+    lastTotalSys = totalSys;
+    lastTotalIdle = totalIdle;
+
+    return percent;
+}
+
+
+static void isMemoryRangeAccepted(int threshold)
+{
+
+	FILE *f = fopen("/home/edward/Operativos/TareaCorta1/Git/OperativosTareaCorta1/EjemploDemonio/trackermon.log", "a");
+		if (f == NULL)
+		{
+		    printf("Error opening file!\n");
+		    exit(1);
+		}
+	
+		    
+
+	sysinfo (&memInfo);
+	long long totalVirtualMem = memInfo.totalram;
+
+	//Add other values in next statement to avoid int overflow on right hand side...
+	totalVirtualMem += memInfo.totalswap;
+	totalVirtualMem *= memInfo.mem_unit;
+
+	long long virtualMemUsed = memInfo.totalram - memInfo.freeram;
+	//Add other values in next statement to avoid int overflow on right hand side...
+	virtualMemUsed += memInfo.totalswap - memInfo.freeswap;
+	virtualMemUsed *= memInfo.mem_unit;
+	
+	double virtualMenAVGD = (virtualMemUsed*100)/totalVirtualMem;
+	
+	if(threshold < virtualMenAVGD){
+		fprintf(f, "[CRITICAL] – Memory Usage is currently %f which is over %d\r\n", virtualMenAVGD, threshold);
+	}
+	fclose(f);
+}
+
+
+
+static void isCPURangeAccepted(int threshold){
+	
+
+	FILE *f = fopen("/home/edward/Operativos/TareaCorta1/Git/OperativosTareaCorta1/EjemploDemonio/trackermon.log", "a");
+		if (f == NULL)
+		{
+		    printf("Error opening file!\n");
+		    exit(1);
+		}
+
+	double CPUusage= getCurrentValue();
+	if(threshold < CPUusage){
+		fprintf(f, "[CRITICAL] – CPU Usage is currently %f which is over %d\r\n", CPUusage, threshold);
+	}
+	fclose(f);
+}
+
+
 int main()
 {
     skeleton_daemon();
 
     while (1)
     {
-        //TODO: Insert daemon code here.
-        syslog (LOG_NOTICE, "First daemon started.");
-	
-        sleep (20);
+		isMemoryRangeAccepted(30);
+		isCPURangeAccepted(2);
+			
+
+		//fprintf(f, "[CRITICAL] – File system /var Usage is currently %d which is over %d\n", 17,5 );
+		
         break;
     }
 
-    syslog (LOG_NOTICE, "First daemon terminated.");
+    syslog (LOG_NOTICE, "DONE");
     closelog();
 
     return EXIT_SUCCESS;
